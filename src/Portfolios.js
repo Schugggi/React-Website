7412fc9d5f74602ab45835c618d1c10650234a81
Schugggi <95489210+Schugggi@users.noreply.github.com>
2022-09-06 15:27:02 +0200

    new featuers and updates

--- src/Portfolios.js
+++ src/Portfolios.js
@@ -1,30 +1,35 @@
 import React from 'react'
 
-import placeholder from './img/placeholder.png'
+import placeholder from './img/placeholder-16-9.jpg'
+import plattformer2 from './img/picture_unity_plattformer_2d.PNG'
 
 export const Portfolios = () => {
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <img src={placeholder} alt={"placeholder"} ></img>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="card shadow">
+            <a href="https://portfolio.bbbaden.ch/view/view.php?t=5wlsGRtmLdeyCKkFOEI7" target="_blank"><img src={plattformer2} alt={"Plattformer Teil 2"}></img></a>
+            <div className="card-body">
+              <p className="card-text">Das ist das Portfolio für den Teil 2 des Unity Plattformers</p>
+            </div>
+          </div>
         </div>
-        <div class="col">
-          One of three columns
+        <div className="col">
+          <div className="card shadow">
+            <img src={placeholder} alt={"placeholder"}></img>
+            <div className="card-body">
+              <p className="card-text">Das ist ein Placeholder</p>
+            </div>
+          </div>
         </div>
-        <div class="col">
-          One of three columns
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <img src={placeholder} alt={"placeholder"} ></img>
-        </div>
-        <div class="col">
-          One of three columns
-        </div>
-        <div class="col">
-          One of three columns
+        <div className="col">
+          <div className="card shadow">
+            <img src={placeholder} alt={"placeholder"}></img>
+            <div className="card-body">
+              <p className="card-text">Das ist ein Placeholder</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
