d029836f122461af84a2e4866e9a00b530897629
Schugggi <95489210+Schugggi@users.noreply.github.com>
2022-10-19 08:12:16 +0200

    header, footer and more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './Header.css'
+import './style.css'
 import { Home } from './Home';
 import { useState } from "react"
 import { Portfolios } from "./Portfolios"
@@ -15,36 +16,101 @@ function App() {
     setSite(site)
   }
 
-  function getClassName(register) {
-    console.log(site === register)
-    if (site === register) { return "nav-link active" }
-    return "nav-link"
+  function changeMenu() {
+    console.log("Button gets klicked")
+    var menu = document.getElementById("menu")
+    if (menu.classList.contains("hidden")) {
+      menu.classList.remove("hidden");
+    } else {
+      menu.classList.add("hidden");
+    }
   }
-
   return (
 
 
     <div className="App">
-      <div className="container">
+      <div className="">
         <header>
-          <ul className="nav">
+          {/* <ul className="nav">
             <li className="nav-item">fridge's Website</li>
             <li className="nav-item"><a onClick={() => handlePress("Home")} className={getClassName("Home")}>Home</a></li>
             <li className="nav-item"><a onClick={() => handlePress("Portfolios")} className={getClassName("Portfolios")}>Portfolios</a></li>
-          </ul>
+          </ul> */}
+
+          <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <div className="flex items-center flex-no-shrink text-white mr-6">
+              <svg className="h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
+              <span className="font-semibold text-xl tracking-tight">fridge's Website</span>
+            </div>
+            <div className="block md:hidden">
+              <button onClick={() => changeMenu()} className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+                <svg className="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+              </button>
+              <div id="menu" className="hidden w-44 rounded dark:bg-teal-500 items-right">
+              <ul className="text-neutral-700 rounded dark:text-neutral-900">
+                <li>
+                  <a onClick={() => handlePress("Home")} className="block py-2 px-2 dark:hover:bg-neutral-700 rounded dark:hover:text-white">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a onClick={() => handlePress("Portfolios")} className="block py-2 px-2 dark:hover:bg-neutral-700 rounded dark:hover:text-white">
+                    Portfolios
+                  </a>
+                </li>
+              </ul>
+            </div>
+            </div>
+
+            
+
+            <div className="w-full block flex-grow md:flex md:items-center md:w-auto">
+              <div className="visibilty text-sm md:flex-grow">
+                <a onClick={() => handlePress("Home")} className="block mt-4 md:inline-block md:mt-0 text-teal-lighter hover:text-white mr-4">
+                  Home
+                </a>
+                <a onClick={() => handlePress("Portfolios")} className="block mt-4 md:inline-block md:mt-0 text-teal-lighter hover:text-white mr-4">
+                  Portfolios
+                </a>
+              </div>
+            </div>
+          </nav>
         </header>
+
+        <div id="menu" className="hidden w-44 rounded dark:bg-teal-500">
+          <ul className="text-neutral-700 dark:text-neutral-900">
+            <li><a onClick={() => handlePress("Home")} className="block py-2 px-4 dark:hover:bg-neutral-700 rounded dark:hover:text-white">
+              Home
+            </a>
+            </li>
+            <li><a onClick={() => handlePress("Portfolios")} className="block py-2 px-4 dark:hover:bg-neutral-700 neutral dark:hover:text-white">
+              Portfolios
+            </a>
+            </li>
+          </ul>
+        </div>
 
         {site === "Home" ? <Home></Home> : null}
         {site === "Portfolios" ? <Portfolios></Portfolios> : null}
 
       </div>
-      <footer className="stickyfooter">
+      {/* <footer className="stickyfooter">
         <div className="container">
           <ul class="nav justify-content-center border-bottom pb-3 mb-3">
             <li className="nav-item"><a className="nav-link px-2 text-muted" onClick={() => handlePress("Home")}>Home</a></li>
             <li className="nav-item"><a className="nav-link px-2 text-muted" onClick={() => handlePress("Portfolios")}>Portfolios</a></li>
           </ul>
-          <p class="text-center text-muted">&copy; fridge 2022</p>
+          <p className="text-center text-muted">&copy; fridge 2022</p>
+        </div>
+      </footer> */}
+      <footer class="w-full h-16 bg-teal-500 border-t-2 border-white
+            fixed left-0 bottom-0
+            flex justify-center items-center
+            text-white
+            ">
+        <div className="space-x-4">
+          <button className="bg-transparent hover:bg-neutral-700 text-neutral-900 font-semibold hover:text-white py-2 px-4 border border-neutral-900 hover:border-transparent rounded"><a className="" onClick={() => handlePress("Home")}>Home</a></button>
+          <button className="bg-transparent hover:bg-neutral-700 text-neutral-900 font-semibold hover:text-white py-2 px-4 border border-neutral-900 hover:border-transparent rounded"><a className="" onClick={() => handlePress("Portfolios")}>Portfolios</a></button>
         </div>
       </footer>
     </div>
--- src/Portfolios.js
+++ src/Portfolios.js
@@ -2,33 +2,40 @@ import React from 'react'
 
 import placeholder from './img/placeholder-16-9.jpg'
 import plattformer2 from './img/picture_unity_plattformer_2d.PNG'
+import plattformer1 from './img/picture_unity_plattformer_2d_teil1.PNG'
 
 export const Portfolios = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <div className="card shadow">
-            <a href="https://portfolio.bbbaden.ch/view/view.php?t=5wlsGRtmLdeyCKkFOEI7" target="_blank"><img src={plattformer2} alt={"Plattformer Teil 2"}></img></a>
-            <div className="card-body">
-              <p className="card-text">Das ist das Portfolio für den Teil 2 des Unity Plattformers</p>
-            </div>
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 m-4 ">
+      <div className="shadow-md shadow-gray-400 rounded">
+        <div className="">
+          <a className="" href="https://portfolio.bbbaden.ch/view/view.php?t=w9HjVEn0Lkoq7dFMSv4s" target="_blank"><img src={plattformer1} alt={"Plattformer Teil 1"}></img></a>
+          <div className="">
+            <p className="">Das ist das Portfolio für den Teil 1 des Unity Plattformers</p>
           </div>
         </div>
-        <div className="col">
-          <div className="card shadow">
-            <img src={placeholder} alt={"placeholder"}></img>
-            <div className="card-body">
-              <p className="card-text">Das ist ein Placeholder</p>
-            </div>
+      </div>
+      <div className="shadow-md shadow-gray-400 rounded">
+        <div className="">
+          <a className="object-scale-down" href="https://portfolio.bbbaden.ch/view/view.php?t=5wlsGRtmLdeyCKkFOEI7" target="_blank"><img src={plattformer2} alt={"Plattformer Teil 2"}></img></a>
+          <div className="">
+            <p className="">Das ist das Portfolio für den Teil 2 des Unity Plattformers</p>
           </div>
         </div>
-        <div className="col">
-          <div className="card shadow">
-            <img src={placeholder} alt={"placeholder"}></img>
-            <div className="card-body">
-              <p className="card-text">Das ist ein Placeholder</p>
-            </div>
+      </div>
+      <div className="shadow-md shadow-gray-400 rounded">
+        <div className="">
+          <img src={placeholder} alt={"placeholder"}></img>
+          <div className="">
+            <p className="">Das ist ein Placeholder</p>
+          </div>
+        </div>
+      </div>
+      <div className="shadow-md shadow-gray-400 rounded">
+        <div className="">
+          <img src={placeholder} alt={"placeholder"}></img>
+          <div className="">
+            <p className="">Das ist ein Placeholder</p>
           </div>
         </div>
       </div>
